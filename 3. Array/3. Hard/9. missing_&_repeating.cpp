e1ce9a1c3466c70e773164c9bb88dca275b0d135
idityaGE <am44910606@gmail.com>
2024-10-01 22:44:54 +0530

    bbb

--- 3. Array/3. Hard/9. missing_&_repeating.cpp
+++ 3. Array/3. Hard/9. missing_&_repeating.cpp
@@ -11,8 +11,26 @@ pair<int, int> repeating_and_missing(vector<int> &arr) {
     if (vc[i] == 2) pp.first = i;
     if (vc[i] == 0) pp.second = i;
   }
-  cout << pp.first << " " << pp.second << endl;
   return pp;
+}
+
+pair<int, int> findMissingRepeatingNumbers(vector<int>& arr) {
+  long long n = arr.size();
+  // s - sn
+  // s2 - s2n
+  long long sn = (n * (n + 1)) / 2;
+  long long s2n = (n * (n + 1) * (2 * n + 1)) / 6;
+  long long s = 0, s2 = 0;
+  for (int i = 0; i < n; i++) {
+    s += arr[i];
+    s2 += ((long long)arr[i] * (long long)arr[i]);
+  }
+  long long val1 = s - sn;
+  long long val2 = s2 - s2n;
+  val2 = val2 / val1;
+  long long rep = (val1 + val2) / 2;
+  long long mis = rep - val1;
+  return {(int)rep, (int)mis};
 }
 
 int main() {
@@ -21,6 +39,7 @@ int main() {
   vector<int> arr(n);
   for (int i = 0; i < n; i++)
     cin >> arr[i];
-  pair<int, int> pp = repeating_and_missing(arr);
+  pair<int, int> pp = findMissingRepeatingNumbers(arr);
+  cout << pp.first << " " << pp.second << endl;
   return 0;
 }
