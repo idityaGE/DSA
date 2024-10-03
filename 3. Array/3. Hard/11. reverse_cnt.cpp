eec6ef39484fdcbcbf27d34877bf57adc2641c6b
idityaGE <am44910606@gmail.com>
2024-10-03 15:55:42 +0530

    11 hard array  is done

--- 3. Array/3. Hard/11. reverse_cnt.cpp
+++ 3. Array/3. Hard/11. reverse_cnt.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int merge(vector<int> &arr, int low, int mid, int high) {
-  int cnt = 0;
+void merge(vector<int> &arr, int low, int mid, int high) {
   vector<int> temp;
   int left = low;
   int right = mid + 1;
@@ -10,7 +9,7 @@ int merge(vector<int> &arr, int low, int mid, int high) {
     if (arr[left] <= arr[right])
       temp.push_back(arr[left++]);
     else
-      temp.push_back(arr[right++]), cnt += (mid - left + 1);
+      temp.push_back(arr[right++]);
   }
   while (left <= mid)
     temp.push_back(arr[left++]);
@@ -18,16 +17,29 @@ int merge(vector<int> &arr, int low, int mid, int high) {
     temp.push_back(arr[right++]);
   for (int i = low; i <= high; i++)
     arr[i] = temp[i - low];
+}
+
+int countPair(vector<int> &arr, int low, int mid, int high) {
+  int cnt = 0;
+  int right = mid + 1;
+  for (int i = low; i <= mid; i++) {
+    while (right <= high && arr[i] > 2 * arr[right])
+      right++;
+    cnt += (right - (mid + 1));
+  }
   return cnt;
 }
 
 int merge_sort(vector<int> &arr, int low, int high) {
-  static int cnt = 0;
-  if (low >= high) return cnt;
+  int cnt = 0;
+  if (low >= high)
+    return cnt;
   int mid = (low + high) / 2;
-  merge_sort(arr, low, mid);
-  merge_sort(arr, mid + 1, high);
-  cnt += merge(arr, low, mid, high);
+  cnt += merge_sort(arr, low, mid);
+  cnt += merge_sort(arr, mid + 1, high);
+  cnt += countPair(arr, low, mid, high);
+  merge(arr, low, mid, high);
+  return cnt;
 }
 
 int main() {
