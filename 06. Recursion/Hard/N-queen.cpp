b2de4f7411992b46c5f0800fb236ba163affc6f3
idityaGE <am44910606@gmail.com>
2024-11-05 19:47:35 +0530

    hashsing method N- Quuen

--- 06. Recursion/Hard/N-queen.cpp
+++ 06. Recursion/Hard/N-queen.cpp
@@ -58,9 +58,50 @@ void printChessBoard(const vector<string>& board) {
   }
 }
 
+// better
+void solve_hash(vector<vector<string>>& ans, vector<string>& board,
+                vector<int> leftRow, vector<int> lowerDia, vector<int> upperDia,
+                int col = 0) {
+  if (col == board.size()) {
+    ans.push_back(board);
+    return;
+  }
+
+  for (int row = 0; row < board.size(); row++) {
+    if (leftRow[row] == 0 && lowerDia[col + row] == 0 &&
+        upperDia[board.size() - 1 + col - row] == 0) {
+      leftRow[row] = 1;
+      lowerDia[col + row] = 1;
+      upperDia[board.size() - 1 + col - row] = 1;
+
+      board[row][col] = 'Q';
+      solve_hash(ans, board, leftRow, lowerDia, upperDia, col + 1);
+      board[row][col] = '.';
+
+      leftRow[row] = 0;
+      lowerDia[col + row] = 0;
+      upperDia[board.size() - 1 + col - row] = 0;
+    }
+  }
+}
+
+vector<vector<string>> solveNQueens_hash(int n) {
+  vector<vector<string>> ans;
+  vector<string> board(n);
+  string s(n, '.');
+  for (int i = 0; i < n; i++)
+    board[i] = s;
+  // hashing
+  vector<int> leftRow(n, 0);
+  vector<int> lowerDia((2 * n - 1), 0);
+  vector<int> upperDia((2 * n - 1), 0);
+
+  solve_hash(ans, board, leftRow, lowerDia, upperDia);
+  return ans;
+}
+
 int main() {
   int n;
-  cout << "Enter the size of the chessboard (n): ";
   cin >> n;
 
   vector<vector<string>> ans;
@@ -69,10 +110,13 @@ int main() {
   for (int i = 0; i < n; i++)
     board[i] = s;
 
-  solve(ans, board);
+  // solve(ans, board);
+
+  vector<vector<string>> ans_hash = solveNQueens_hash(n);
+
 
   int solution_num = 1;
-  for (const auto& solution : ans) {
+  for (const auto& solution : ans_hash) {
     cout << "\nSolution " << solution_num++ << ":\n";
     printChessBoard(solution);
     cout << endl;
@@ -80,8 +124,6 @@ int main() {
 
   return 0;
 }
-
-
 
 /*
 Input: n = 4
