827e1cebeaad04b495bcb23d4f3098bb537489e7
idityaGE <am44910606@gmail.com>
2024-11-08 20:47:57 +0530

    bit manu started

---
+++ 07. Bit Manipulation/Basics/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string DecimalToBinary(int n) {
+  string ans = "";
+  while (n != 1) {
+    if (n % 2 == 1)
+      ans += '1';
+    else
+      ans += '0';
+    n >>= 1;  // n /= 1
+  }
+  ans += '1';
+  reverse(ans.begin(), ans.end());
+  return ans;
+}
+
+int BinaryToDecimal(string s) {
+  int ans = 0;
+  int len = s.length();
+  int binary = stoi(s);
+  for (int i = len - 1; i >= 0; i--) {
+    ans += (binary % 10) * pow(2, len - i - 1);
+    binary /= 10;
+  }
+  return ans;
+}
+
+// AND --> &
+
+// OR --> |
+
+// XOR --> ^
+//   1 ^ 1 = 0  ==> even no. of n then 0
+//   1 ^ 0 = 1  ==> odd no. of n then 1
+
+// RIGHT SWIFT --> >>
+//    x >> k == x / pow(2, k);
+
+// LEFT SWIFT --> <<
+//    x << k == x * pow(2, k);
+
+// NOT --> ~
+
+
+int main() {
+  cout << DecimalToBinary(13);
+  cout << endl;
+  cout << BinaryToDecimal("1001");
+  return 0;
+}
