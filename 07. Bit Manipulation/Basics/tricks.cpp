7fec4643ffdb2dc02232a6be9f3608e28b52383f
idityaGE <am44910606@gmail.com>
2024-11-11 16:50:04 +0530

    start

--- 07. Bit Manipulation/Basics/tricks.cpp
+++ 07. Bit Manipulation/Basics/tricks.cpp
@@ -41,6 +41,10 @@ int removeLastSetBit(int n) {
   return n & (n - 1);
 }
 
+int setLastUnsetBit(int n) {
+  return n | (n + 1);
+}
+
 // check if numbers is power 2 or not
 // trick - observe its binary will have only one 1's bit
 // n = 32 = 100000, n = 16 = 10000
