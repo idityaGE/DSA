7c99ef3389c1bddb8d5d14dcc65632d349d3cd97
idityaGE <am44910606@gmail.com>
2024-11-15 20:36:36 +0530

    ff

--- 02. Sorting/selection_sort.cpp
+++ 02. Sorting/selection_sort.cpp
@@ -10,10 +10,10 @@ void selection_sort(int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
     int min_idx = i;
     for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[min])
-        min = j;
+      if (arr[j] < arr[min_idx])
+        min_idx = j;
     }
-    swap(arr[min], arr[i]);
+    swap(arr[min_idx], arr[i]);
   }
 }
 
@@ -24,15 +24,7 @@ int main() {
   for (int i = 0; i < n; i++)
     cin >> arr[i];
 
-  // Selection Sort
-  for (int i = 0; i < n - 1; i++) {
-    int min = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[min])
-        min = j;
-    }
-    swap(arr[min], arr[i]);
-  }
+  selection_sort(arr, n);
 
   cout << "After Sort : " << endl;
   print(arr, n);
