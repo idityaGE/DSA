10febf45fc5498cd8d149c413b7009a5c7c8fa7c
idityaGE <am44910606@gmail.com>
2024-11-20 16:24:08 +0530

    stack LL is done

--- 08. Stack_&_Queue/00. Basic/stack.cpp
+++ 08. Stack_&_Queue/00. Basic/stack.cpp
@@ -2,45 +2,76 @@
 using namespace std;
 
 class Stack {
-  int size;
-  int top;
-  int *s;
+  int size;   // Maximum size of the stack
+  int top;    // Index of the top element
+  int *s;     // Pointer to the stack array
 
  public:
-  Stack(int size)
-      : size(size), top(-1), s(new int[size]) {}
+  // Constructor
+  Stack(int stackSize)
+      : size(stackSize), top(-1), s(new int[stackSize]) {}
 
+  // Push an element onto the stack
   void push(int val) {
     if (top == size - 1)
       throw runtime_error("Stack is full");
     s[++top] = val;
   }
 
+  // Pop an element from the stack
   int pop() {
     if (isEmpty())
       throw runtime_error("Stack is empty");
     return s[top--];
   }
 
-  int top() {
+  // Get the top element of the stack
+  int getTop() const {
     if (isEmpty())
       throw runtime_error("Stack is empty");
     return s[top];
   }
 
-  bool isEmpty() {
+  // Check if the stack is empty
+  bool isEmpty() const {
     return top == -1;
   }
 
-  int size() {
+  // Get the current number of elements in the stack
+  int getSize() const {
     return top + 1;
   }
 
+  // Destructor
   ~Stack() {
     delete[] s;
   }
 };
 
 int main() {
+  try {
+    Stack st(5);  // Create a stack with size 5
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << "Top element: " << st.getTop() << endl;
+    cout << "Stack size: " << st.getSize() << endl;
+
+    cout << "Popping: " << st.pop() << endl;
+    cout << "Stack size after pop: " << st.getSize() << endl;
+
+    cout << "Popping all elements..." << endl;
+    st.pop();
+    st.pop();
+
+    // Uncommenting the next line will throw an error
+    // st.pop();
+
+    cout << "Is stack empty? " << (st.isEmpty() ? "Yes" : "No") << endl;
+  } catch (const exception &e) {
+    cerr << "Error: " << e.what() << endl;
+  }
+
   return 0;
 }
--- 08. Stack_&_Queue/00. Basic/stack_LL.cpp
+++ 08. Stack_&_Queue/00. Basic/stack_LL.cpp
@@ -2,31 +2,105 @@
 using namespace std;
 
 class Node {
-  int data;
-  int *next;
-
  public:
-  Node(int val)
-      : data(val), next(nullptr) {}
+  int data;
+  Node *next;
+  Node(int val) : data(val), next(nullptr) {}
 };
 
 class Stack {
   Node *top;
 
  public:
-  Stack()
-      : top(nullptr) {}
-  Stack(int val)
-      : top(new Node(val)) {}
+  Stack() : top(nullptr) {}
+  
+  Stack(int val) : top(new Node(val)) {}
 
-  ~Stack() {
-    delete top;
+  void push(int val) {
+    Node *t = new Node(val);
+    t->next = top;
+    top = t;
   }
 
+  int pop() {
+    if (isEmpty())
+      throw runtime_error("Stack is empty");
+    Node *t = top;
+    int x = t->data;
+    top = top->next;
+    delete t;
+    return x;
+  }
 
-  
+  int getTop() const {
+    if (isEmpty())
+      throw runtime_error("Stack is empty");
+    return top->data;
+  }
+
+  bool isEmpty() const {
+    return top == nullptr;
+  }
+
+  void display() const {
+    if (isEmpty()) {
+      cout << "Stack is empty" << endl;
+      return;
+    }
+    Node *p = top;
+    while (p != nullptr) {
+      cout << p->data << " | ";
+      p = p->next;
+    }
+    cout << endl;
+  }
+
+  int peek(int index) const {
+    if (isEmpty())
+      throw runtime_error("Stack is empty");
+    Node *t = top;
+    for (int i = 0; t != nullptr && i < index - 1; i++)
+      t = t->next;
+    if (t == nullptr)
+      throw runtime_error("Invalid index");
+    return t->data;
+  }
+
+  ~Stack() {
+    while (top != nullptr) {
+      Node *temp = top;
+      top = top->next;
+      delete temp;
+    }
+  }
 };
 
 int main() {
+  try {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    cout << "Top element: " << s.getTop() << endl;
+    cout << "Peek at index 2: " << s.peek(2) << endl;
+
+    cout << "Popping: " << s.pop() << endl;
+    s.display();
+
+    cout << "Popping all elements..." << endl;
+    s.pop();
+    s.pop();
+
+    // Uncommenting the next line will throw an error
+    // s.pop();
+
+    s.display();
+  } catch (const exception &e) {
+    cerr << e.what() << endl;
+  }
+
   return 0;
 }
