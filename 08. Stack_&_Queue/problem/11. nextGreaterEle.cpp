a05ade6ad9f72752fcef32378c9935c7de9b4c38
idityaGE <am44910606@gmail.com>
2024-11-25 11:16:04 +0530

    nex greater element 1 is done

---
+++ 08. Stack_&_Queue/problem/11. nextGreaterEle.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+  unordered_map<int, int> mp;
+  stack<int> st;
+  vector<int> ans;
+
+  for (int i = nums2.size() - 1; i >= 0; i--) {
+    while (!st.empty() && st.top() < nums2[i])
+      st.pop();
+    if (st.empty())
+      mp[nums2[i]] = -1;
+    else
+      mp[nums2[i]] = st.top();
+    st.push(nums2[i]);
+  }
+
+  for (auto i : nums1)
+    ans.push_back(mp[i]);
+
+  return ans;
+}
+
+int main() {
+  return 0;
+}
